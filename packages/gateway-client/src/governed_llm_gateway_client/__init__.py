56b48582753d61b9d2d7c853579d840368c93cac
Bruno Vicco <bfvicco@gmail.com>
2026-09-03 21:26:35 -0300

    feat: export Phase 12 client errors

--- packages/gateway-client/src/governed_llm_gateway_client/__init__.py
+++ packages/gateway-client/src/governed_llm_gateway_client/__init__.py
@@ -1,5 +1,22 @@
-"""Thin client SDK boundary."""
+"""Thin provider-neutral client SDK boundary."""
 
 from .client import GatewayClient, GatewayClientConfig
+from .errors import (
+    GatewayClientError,
+    GatewayConfigurationError,
+    GatewayHTTPError,
+    GatewayProtocolError,
+    GatewayRequestError,
+    GatewayTransportError,
+)
 
-__all__ = ["GatewayClient", "GatewayClientConfig"]
+__all__ = [
+    "GatewayClient",
+    "GatewayClientConfig",
+    "GatewayClientError",
+    "GatewayConfigurationError",
+    "GatewayHTTPError",
+    "GatewayProtocolError",
+    "GatewayRequestError",
+    "GatewayTransportError",
+]
