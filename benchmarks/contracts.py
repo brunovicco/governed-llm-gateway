1f9bc593894d17572a30d7a9be9eebec3185a938
Bruno Vicco <bfvicco@gmail.com>
2026-09-06 01:04:34 -0300

    feat: register security analysis workload

--- benchmarks/contracts.py
+++ benchmarks/contracts.py
@@ -20,6 +20,7 @@ class BenchmarkWorkload(StrEnum):
     RAG_PTBR = "rag_ptbr"
     CODE_GENERATION = "code_generation"
     CODE_REVIEW = "code_review"
+    SECURITY_ANALYSIS = "security_analysis"
     TOOL_USE = "tool_use"
     AGENT_ORCHESTRATION = "agent_orchestration"
     MULTIMODAL_ANALYSIS = "multimodal_analysis"
