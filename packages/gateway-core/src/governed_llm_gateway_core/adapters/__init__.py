fc0bbc5289f7c4dbff95b7ddb3a9f136b1846577
Bruno Vicco <bfvicco@gmail.com>
2026-09-07 01:15:19 -0300

    feat: export provider runtime composition primitives

--- packages/gateway-core/src/governed_llm_gateway_core/adapters/__init__.py
+++ packages/gateway-core/src/governed_llm_gateway_core/adapters/__init__.py
@@ -11,17 +11,35 @@ from .model_registry_yaml import load_model_registry, load_model_registry_text
 from .openai_compatible import OpenAICompatibleAdapter
 from .openai_responses import OpenAIResponsesAdapter
 from .policy_router import PolicyRouterHttpAdapter, StdlibPolicyTransport
+from .provider_runtime import (
+    EnvironmentProviderSecretResolver,
+    OpenAICompatibleRuntimeOptions,
+    ProviderApiFamily,
+    ProviderRuntimeConfig,
+    ProviderRuntimeConfigurationError,
+    ProviderSecretResolutionError,
+    ProviderSecretResolver,
+    build_static_provider_resolver,
+)
 from .ranking_policy_yaml import load_ranking_policy, load_ranking_policy_text
 
 __all__ = [
     "AnthropicMessagesAdapter",
+    "EnvironmentProviderSecretResolver",
     "GeminiAdapter",
     "GovernanceAuthorizationVerificationError",
     "OpenAICompatibleAdapter",
+    "OpenAICompatibleRuntimeOptions",
     "OpenAIResponsesAdapter",
     "PolicyRouterHttpAdapter",
+    "ProviderApiFamily",
+    "ProviderRuntimeConfig",
+    "ProviderRuntimeConfigurationError",
+    "ProviderSecretResolutionError",
+    "ProviderSecretResolver",
     "StaticGovernanceKeyResolver",
     "StdlibPolicyTransport",
+    "build_static_provider_resolver",
     "load_model_registry",
     "load_model_registry_text",
     "load_ranking_policy",
